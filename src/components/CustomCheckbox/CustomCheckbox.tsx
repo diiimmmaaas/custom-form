6b01a881e6cceb503b9a809c4bb0501e38efb13b
diiimmmaaas <dm.sokolov1994@gmail.com>
2023-04-27 10:07:21 +0300

    fix layout

--- src/components/CustomCheckbox/CustomCheckbox.tsx
+++ src/components/CustomCheckbox/CustomCheckbox.tsx
@@ -22,10 +22,9 @@ export const CustomCheckbox: FC<CustomCheckboxPropsType> = ({
         <div className={styles.checkboxesContainer}>
           {options.map((option, index) => {
             return <div key={option.id} className={styles.checkboxContainer}>
-              <Field className={styles.checkbox} id="formBox" type="checkbox" name={name}
+              <Field className={styles.checkbox} type="checkbox" name={name}
                      value={option.title}/>
-              <label className={styles.checkboxLabel}
-                     htmlFor="formBox">{option.title}</label>
+              <label className={styles.checkboxLabel}>{option.title}</label>
             </div>;
           })}
         </div>
